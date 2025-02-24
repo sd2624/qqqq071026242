b3e0e4250334c4464bc4e3b8c6988d1a038fc3d7
sd2624 <sd2624@naver.com>
2025-02-24 21:22:45 +0900

    Update auto_band_action.py

--- auto_band_action.py
+++ auto_band_action.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys  # Keys import 추가
 import os
 import json
 import time
